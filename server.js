95f28de744542b477f65f3b0dd1e2616fd1eaeec
jcostaaaaa <100879576+jcostaaaaa@users.noreply.github.com>
2023-12-01 13:42:52 +0000

    logs, tests and fixed bugs

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const winston = require("winston");
+require("winston-mongodb");
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 
 const uri = process.env.ATLAS_URI;
+const uriLogs = process.env.ATLAS_URI_LOGS;
+const app = express();
+
 mongoose.Promise = global.Promise;
 mongoose
-  .connect(uri)
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Successfully connected to MongoDB.");
   })
@@ -16,19 +22,52 @@ mongoose
     console.error("Connection error", err);
   });
 
-// Midleware
-const app = express();
-app.use("/", express.static(path.join(__dirname, "static")));
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.MongoDB({
+      db: uriLogs,
+      options: { useNewUrlParser: true, useUnifiedTopology: true },
+    }),
+  ],
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.simple()
+  ),
+});
+
+app.use((req, res, next) => {
+  const authorizationHeader = req.headers["authorization"];
+  const userToken = authorizationHeader
+    ? authorizationHeader.split(" ")[1]
+    : "N/A";
+
+  console.log(userToken);
+  if (userToken !== "N/A") {
+    const userLogged = jwt.verify(userToken, process.env.TOKEN_SECRET);
+    const idUserLogged = userLogged?.id;
+
+    const logMessage =
+      userToken === "N/A"
+        ? `HTTP ${req.method} request - Path: ${req.path}`
+        : `HTTP ${req.method} request - Path: ${req.path}, UserID: ${idUserLogged},User Token: ${userToken}`;
+    logger.info(logMessage);
+  } else {
+    const logMessage = `HTTP ${req.method} request - Path: ${req.path}`;
+    logger.info(logMessage);
+  }
+
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// routes
 app.use("/user", require("./src/routes/authRoutes"));
 
-// documentation
 app.use("/apidocjs", express.static(path.join(__dirname, "apidoc")));
 
-let port = 9998;
+let port = 9999;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
