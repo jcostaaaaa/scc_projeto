e9df3944617e9677384271371ccc65c6ac35de04
Daniel Miranda <danielmiranda7@hotmail.com>
2023-12-16 17:51:28 +0000

    bugs fix

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 describe("Auth", () => {
   it("should return 400 with a token", async () => {
     try {
-      const response = await axios.post("http://localhost:9998/user/login", {
+      const response = await axios.post("http://localhost:9999/user/login", {
         username: "admin",
       });
 
@@ -19,7 +19,7 @@ describe("Auth", () => {
 
 describe("Auth", () => {
   it("should return 200 with a token", async () => {
-    const response = await axios.post("http://localhost:9998/user/login", {
+    const response = await axios.post("http://localhost:9999/user/login", {
       username: "admin",
       password: "admin123",
     });
@@ -33,12 +33,12 @@ describe("Auth", () => {
 describe("Auth", () => {
   it("should return 400 with a token", async () => {
     try {
-      const response = await axios.post("http://localhost:9998/user/login", {
+      const response = await axios.post("http://localhost:9999/user/login", {
         username: "admin",
         password: "admin1234",
       });
     } catch (error) {
-      expect(error.response.status).toBe(400); 
+      expect(error.response.status).toBe(400);
       expect(error.response.data.message).toBe("invalid credentials");
     }
   });
